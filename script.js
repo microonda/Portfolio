913f997d7c312b25c6ba90c78a0a7dfbdfffa9f9
Felipe <felipeborgesmooca@gmail.com>
2023-12-26 17:36:20 -0300

    Resolução de Bug nos links ao passar pro GithubPages

--- script.js
+++ script.js
@@ -13,18 +13,21 @@ function copiarTelefone() { copiar('(11) 93388-7185', 'Telefone (whatsapp)') }
 
 // aparecer sections
 
-window.onscroll = function() {scrollFuncao()};
-
-function scrollFuncao() {
-    voltar = document.getElementById("voltar")
-    voltar.style.transition = "0.3s";
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        voltar.style.display = "block";
-    } 
-    else {
-        voltar.style.display = "none";
-    }
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+  
+    const isTopInViewport = e => e.getBoundingClientRect().top <= (window.innerHeight || document.documentElement.clientHeight);
+    const handleScroll = () => sections.forEach(sec => isTopInViewport(sec) ? showSection(sec) : null);
+  
+    const showSection = sec => {
+      sec.style.display = 'block';
+      sec.style.opacity = '1';
+      sec.style.transform = 'translateY(0)';
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+  });
   
 window.onscroll = function() {scrollFuncao()};
 
@@ -39,7 +42,7 @@ function scrollFuncao() {
     }
 }
 
-// Quando o usuário clicar no botão, voltar para o topo da página
+// voltar para o topo da página
 function topFuncao() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0
